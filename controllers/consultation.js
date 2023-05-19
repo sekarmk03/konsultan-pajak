35c3b5f29ed74e74dd62187920914f983e81c59e
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2023-05-19 12:58:01 +0700

    Add feat start & end consultation

--- controllers/consultation.js
+++ controllers/consultation.js
@@ -70,7 +70,9 @@ module.exports = {
                 .addLink('self', `${API_BASE_PATH}/consultations/${cons.id}`)
                 .addLink('schedule', `${API_BASE_PATH}/schedules/${cons.schedule_id}`)
                 .addLink('customer', `${API_BASE_PATH}/customers/${cons.schedule.cust_id}`)
-                .addLink('admin', `${API_BASE_PATH}/admins/${cons.admin_id}`);
+                .addLink('admin', `${API_BASE_PATH}/admins/${cons.admin_id}`)
+                .addLink('start', `${API_BASE_PATH}/consultations/${cons.id}/start`)
+                .addLink('end', `${API_BASE_PATH}/consultations/${cons.id}/end`);
 
                 return resource;
             });
@@ -139,7 +141,9 @@ module.exports = {
             .addLink('self', `${API_BASE_PATH}/consultations/${consultation.id}`)
             .addLink('schedule', `${API_BASE_PATH}/schedules/${consultation.schedule_id}`)
             .addLink('customer', `${API_BASE_PATH}/customers/${consultation.schedule.cust_id}`)
-            .addLink('admin', `${API_BASE_PATH}/admins/${consultation.admin_id}`);
+            .addLink('admin', `${API_BASE_PATH}/admins/${consultation.admin_id}`)
+            .addLink('start', `${API_BASE_PATH}/consultations/${consultation.id}/start`)
+            .addLink('end', `${API_BASE_PATH}/consultations/${consultation.id}/end`);
 
             const response = {
                 status: 'OK',
@@ -177,7 +181,9 @@ module.exports = {
             const consResource = halson(created.toJSON())
             .addLink('self', `${API_BASE_PATH}/consultations/${created.id}`)
             .addLink('schedule', `${API_BASE_PATH}/schedules/${created.schedule_id}`)
-            .addLink('admin', `${API_BASE_PATH}/admins/${created.admin_id}`);
+            .addLink('admin', `${API_BASE_PATH}/admins/${created.admin_id}`)
+            .addLink('start', `${API_BASE_PATH}/consultations/${created.id}/start`)
+            .addLink('end', `${API_BASE_PATH}/consultations/${created.id}/end`);
 
             const response = {
                 status: 'CREATED',
@@ -231,7 +237,9 @@ module.exports = {
             const consResource = halson(consultation.toJSON())
             .addLink('self', `${API_BASE_PATH}/consultations/${consultation.id}`)
             .addLink('schedule', `${API_BASE_PATH}/schedules/${consultation.schedule_id}`)
-            .addLink('admin', `${API_BASE_PATH}/admins/${consultation.admin_id}`);
+            .addLink('admin', `${API_BASE_PATH}/admins/${consultation.admin_id}`)
+            .addLink('start', `${API_BASE_PATH}/consultations/${consultation.id}/start`)
+            .addLink('end', `${API_BASE_PATH}/consultations/${consultation.id}/end`);
 
             const response = {
                 status: 'OK',
@@ -274,6 +282,116 @@ module.exports = {
                     self: { href: req.originalUrl },
                     collection: { href: `${API_BASE_PATH}/consultations` },
                     deleted: { href: `${API_BASE_PATH}/consultations/${consultation.id}` }
+                }
+            };
+
+            return res.status(200).json(response);
+        } catch (err) {
+            next(err);
+        }
+    },
+
+    start: async (req, res, next) => {
+        try {
+            const { id } = req.params;
+
+            const consultation = await Consultation.findOne({where: {id}});
+            if (!consultation) {
+                return res.status(404).json({
+                    status: 'NOT_FOUND',
+                    message: `Consultation didn't exist`,
+                    data: null
+                });
+            }
+
+            if (consultation.status != state.NOT_STARTED) {
+                let message = '';
+                switch (consultation.status) {
+                    case state.ONGOING:
+                        message = `Consultation is ongoing`;
+                        break;
+                    case state.DONE:
+                        message = `Consultation already done`;
+                        break;
+                    default:
+                        message = `Consultation status error`;
+                        break;
+                }
+                return res.status(400).json({
+                    status: 'BAD_REQUEST',
+                    message: message,
+                    data: null
+                });
+            }
+
+            await consultation.update({
+                status: state.ONGOING,
+                date_start: new Date()
+            });
+
+            const response = {
+                status: 'OK',
+                message: 'Start consultation success',
+                data: consultation,
+                links: {
+                    self: { href: req.originalUrl },
+                    collection: { href: `${API_BASE_PATH}/consultations` },
+                    updated: { href: `${API_BASE_PATH}/consultations/${consultation.id}` }
+                }
+            };
+
+            return res.status(200).json(response);
+        } catch (err) {
+            next(err);
+        }
+    },
+
+    end: async (req, res, next) => {
+        try {
+            const { id } = req.params;
+
+            const consultation = await Consultation.findOne({where: {id}});
+            if (!consultation) {
+                return res.status(404).json({
+                    status: 'NOT_FOUND',
+                    message: `Consultation didn't exist`,
+                    data: null
+                });
+            }
+
+            if (consultation.status != state.ONGOING) {
+                let message = '';
+                switch (consultation.status) {
+                    case state.NOT_STARTED:
+                        message = `Consultation is not started yet`;
+                        break;
+                    case state.DONE:
+                        message = `Consultation already done`;
+                        break;
+                    default:
+                        message = `Consultation status error`;
+                        break;
+                }
+                return res.status(400).json({
+                    status: 'BAD_REQUEST',
+                    message: message,
+                    data: null
+                });
+            }
+
+            await consultation.update({
+                status: state.DONE,
+                date_end: new Date()
+            });
+
+            const response = {
+                status: 'OK',
+                message: 'End consultation success',
+                data: consultation,
+                links: {
+                    self: { href: req.originalUrl },
+                    collection: { href: `${API_BASE_PATH}/consultations` },
+                    updated: { href: `${API_BASE_PATH}/consultations/${consultation.id}` }
                 }
             };
 
--- routes/consultation.js
+++ routes/consultation.js
@@ -8,6 +8,8 @@ router.get('/', authorize([roles.SUPERADMIN, roles.ADMIN]), c.consultation.index
 router.get('/:id', authorize([roles.SUPERADMIN, roles.ADMIN]), c.consultation.show);
 router.post('/', authorize(), c.consultation.create);
 router.put('/:id', authorize([roles.SUPERADMIN, roles.ADMIN]), c.consultation.update);
+router.put('/:id/start', authorize([roles.SUPERADMIN, roles.ADMIN]), c.consultation.start);
+router.put('/:id/end', authorize([roles.SUPERADMIN, roles.ADMIN]), c.consultation.end);
 router.delete('/:id', authorize([roles.SUPERADMIN, roles.ADMIN]), c.consultation.delete);
 
 module.exports = router;
