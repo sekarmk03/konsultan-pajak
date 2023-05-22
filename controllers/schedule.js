4fdb9e08a37cc5c93557ae9b5daf7df774ad3650
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2023-05-22 17:01:27 +0700

    Fix notification not defined

--- controllers/schedule.js
+++ controllers/schedule.js
@@ -1,4 +1,4 @@
-const { Schedule, ConsultType, Customer, Consultation } = require('../models');
+const { Schedule, ConsultType, Customer, Consultation, Notification } = require('../models');
 const { Op } = require('sequelize');
 const state = require('../common/constant/status');
 const openHours = require('../common/constant/openHours');
